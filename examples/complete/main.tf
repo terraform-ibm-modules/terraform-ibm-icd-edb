d2a1b039ce459e0254809dfe034ea8a7fc550f6a
shemau <shemau@gmail.com>
2024-03-15 10:47:14 +0000

    chore: Use hashicorp registry (#165)
    Co-authored-by: Steve Peggs <peggs@uk.ibm.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,8 @@
 ##############################################################################
 
 module "resource_group" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.1.5"
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.5"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -14,7 +15,8 @@ module "resource_group" {
 ##############################################################################
 
 module "key_protect_all_inclusive" {
-  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect-all-inclusive.git?ref=v4.8.4"
+  source            = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
+  version           = "4.8.4"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
